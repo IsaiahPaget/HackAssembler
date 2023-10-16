9ead31add2b9bb8a91182d8c79768387fdf346e6
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2023-10-15 20:27:38 -0700

    opened and read from file

--- src/main.c
+++ src/main.c
@@ -1,24 +1,73 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-FILE OpenFile(char* filePath) 
+FILE* OpenFile(char* filePath) 
 {
-	bool isSuccess = false;
-	FILE *fptr;
+	printf("Initiate OpenFile\n");
+	FILE* fptr;
 	fptr = fopen(filePath, "r");
 
 	if (fptr == NULL)
 	{
-		printf("Error, file not opened");
+		perror("Error, could not open file\n");
 		exit(1);
 	}
 
-	return *fptr; 
+	return fptr; 
+}
+
+char* ReadFile(FILE* file) 
+{
+	printf("Intiate ReadFile\n");
+	char* code;
+	char* contents;
+	int index = 0;
+	int asci;
+
+	// allocate memory by size of file
+	fseek(file, 0, SEEK_END);
+	long fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	code = malloc(fileSize);
+
+	// read the contents of the file into an array
+	while ((asci = fgetc(file)) != EOF)
+	{
+		code[index] = (char)asci;
+		index++;
+	}
+
+	code[index] = '\0';
+
+	fclose(file);
+
+	strcpy(contents, code);
+
+	free(code);
+
+	return contents;
 }
 
 int main(int argc, char *argv[])
 {
-	FILE openedFile = OpenFile("./file.txt");
+	// Make sure user enters correct number of arguments
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: ./HackAssembler filename\n");
+		return 1;
+	}
+
+	FILE* openedFile = OpenFile(argv[1]);
+	char* fileContents = ReadFile(openedFile);
+
+
+	for (int i = 0; fileContents[i] != EOF; i++) 
+	{
+		printf("%c", fileContents[i]);
+	
+	}
+
 	return 0; 
 }
