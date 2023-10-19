a1787c440301210bc1a2c895595b3e4b82015a5f
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2023-10-18 22:53:44 -0700

    got the reading from a file working properly

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MORE_LINES 1024
+#define MORE_CHARS 1024
+
 FILE* OpenFile(char* filePath) 
 {
 	printf("Initiate OpenFile\n");
 	FILE* fptr;
-	fptr = fopen(filePath, "r");
+	fptr = fopen(filePath, "rb");
 
 	if (fptr == NULL)
 	{
@@ -18,36 +22,77 @@ FILE* OpenFile(char* filePath)
 	return fptr; 
 }
 
-char* ReadFile(FILE* file) 
+char** ReadFile(FILE *file) 
 {
-	printf("Intiate ReadFile\n");
-	char* code;
-	char* contents;
-	int index = 0;
-	int asci;
+	printf("Initiate ReadFile\n");
+	
+	char **lines = malloc(sizeof(char *) * MORE_LINES);
 
-	// allocate memory by size of file
-	fseek(file, 0, SEEK_END);
-	long fileSize = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	code = malloc(fileSize);
+	size_t total_lines = 0;
+	size_t total_chars = 0;
 
-	// read the contents of the file into an array
-	while ((asci = fgetc(file)) != EOF)
+	char c;
+	
+	while (!feof(file))
 	{
-		code[index] = (char)asci;
-		index++;
+		if (ferror(file))
+		{
+			printf("Error reading from file\n");
+			exit(1);
+		}
+
+		// get first character of file
+		c = fgetc(file);
+		
+		// if first character read than allocate space for more characters
+		if (total_chars == 0) 
+		{
+			lines[total_lines] = malloc(MORE_CHARS); 
+		}
+		
+		// assign character to the current spot in array of strings
+		lines[total_lines][total_chars] = c;
+
+		// move onto next character
+		total_chars++;
+		
+		// if next line than reallocate new space for the terminator charactor to finish the strings
+		if (c == '\n')
+		{
+			lines[total_lines] = realloc(lines[total_lines], total_chars + 1);
+			lines[total_lines][total_chars] = '\0';
+			
+			// move onto the next line
+			total_lines++;
+			total_chars = 0;
+
+			// if need more lines than add reallocate MORE_LINES
+			if (total_lines % MORE_LINES == 0)
+			{
+				size_t new_size = total_lines + MORE_LINES;
+				lines = realloc(lines, sizeof(char *) * new_size);
+			}
+		}
+		else if (total_chars % MORE_CHARS == 0) 
+		{
+			size_t new_size = total_chars + MORE_CHARS;
+			lines[total_lines] = realloc(lines[total_lines],new_size);
+		}
+
 	}
 
-	code[index] = '\0';
+	if (total_chars != 0)
+	{
+		lines[total_lines] = realloc(lines[total_lines], total_chars + 1);
+		lines[total_lines][total_chars] = '\0';
+		
+		// move onto the next line
+		total_lines++;
+	}
 
-	fclose(file);
+	lines = realloc(lines, sizeof(char *) * total_lines);
 
-	strcpy(contents, code);
-
-	free(code);
-
-	return contents;
+	return lines;
 }
 
 int main(int argc, char *argv[])
@@ -60,14 +105,16 @@ int main(int argc, char *argv[])
 	}
 
 	FILE* openedFile = OpenFile(argv[1]);
-	char* fileContents = ReadFile(openedFile);
+	char** fileContents = ReadFile(openedFile);
 
 
-	for (int i = 0; fileContents[i] != EOF; i++) 
+
+	for (int i = 0; *fileContents[i] != EOF; i++) 
 	{
-		printf("%c", fileContents[i]);
-	
+		printf("%s", fileContents[i]);
 	}
+
+	free(fileContents);
 
 	return 0; 
 }
